14de939160a6170488e23aa2973480e4b9df0e5a
elisehein <elisehein@gmail.com>
2011-03-07 14:39:14 -0800

    again

--- pages/home.php
+++ pages/home.php
@@ -22,21 +22,6 @@ $PAGE['content'] = '
 
 {include:/Bernie/templates/FrontPageLoggedOut.html}
 
-<!--
-<h1>Login</h1>
-<form name="login" action="?p=login" method="post">
-    Username: <input type="text" name="username" />
-    Password: <input type="password" name="password" />
-    <input type="submit" value="Login" />
-</form>
-<h1>Register</h1>
-<form name="register" action="?p=register" method="post">
-    Username: <input type="text" name="username" maxlength="30" />
-    Password: <input type="password" name="pass1" />
-    Password Again: <input type="password" name="pass2" />
-    <input type="submit" value="Register" />
-</form>';
--->
 $PAGE['content'] .= '<br /><br />Congrats you found home... Would you like to see a <a href="?p=demoPegParse">pegParseDemo</a>?!?';
 
 ?>
