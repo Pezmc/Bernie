5dfce505c5dc6789898955c6eac8bc740e82ad4e
D1Adam <adam-gill@hotmail.co.uk>
2011-03-10 14:43:28 -0800

    that should do something

--- pages/bernie.php
+++ pages/bernie.php
@@ -24,7 +24,7 @@ $PAGE['title'] = "Bernie";
 $suggestion= array("sugImage"=>"http://cvcl.mit.edu/hybrid/cat2.jpg","Cat"=>"_","sugAuthor"=>"Cat writer","sugYear"=>"1990","sugLength"=>"Long","sugSubTItle"=>"cat the cat cat is a cat which shat","sugDescription"=>"cattycattycatty");
 
 
-$PAGE['content'] = parse("Bernie.html");
+$PAGE['content'] = parse("Bernie.html", $suggestion);
 
 
 
