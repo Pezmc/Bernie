7fe2bb25aa789220c1e34525eac1b70985b3e90e
elisehein <elisehein@gmail.com>
2011-03-10 13:22:50 -0800

    added more userinfo

--- globals/user.php
+++ globals/user.php
@@ -14,6 +14,10 @@
 
 $USER['username'] = "";
 $USER['parents_email'] = "";
+$USER['parents_name'] = "";
+$USER['day_of_birth'] = "";
+$USER['month_of_birth'] = "";
+$USER['year_of_birth'] = "";
 $USER['first_name'] = "";
 $USER['last_name'] = "";
 $USER['password'] = "";
