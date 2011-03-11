a26c4c5a0d7f9ff166dea60428c4245cadd3496c
Stephen Hall <halls9@cs.man.ac.uk>
2011-03-11 19:03:09 +0000

    addcomment.php

---
+++ _test/commentingsystem/addcomment.php
@@ -0,0 +1,12 @@
+<?php
+include_once(" connect.php");
+
+$msg_title = $_POST['msg_title'];
+$msg_message = $_POST['msg_message']; 
+$msg_submit = $_POST['msg_submit'];
+if ($msg_submit&&$msg_title&&$msg_message)
+{
+     mysql_query("INSERT INTO comments VALUES (''.'".$msg_title."'.'".$msg_message."')");
+}
+     header("Location: commentsystem/");
+?>
