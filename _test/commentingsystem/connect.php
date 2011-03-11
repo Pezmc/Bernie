14594731197babfb4c4f330c8fb515890cb2a7dc
Stephen Hall <halls9@cs.man.ac.uk>
2011-03-11 15:24:35 +0000

    Test system

---
+++ _test/commentingsystem/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$mysql_id = mysql_connect('ramen.cs.man.ac.uk', 'halls9', 'IM66o9mx');
+mysql_select_db('S09_halls9', $mysql_id);
+
+?>
+
+
---
+++ _test/commentingsystem/index.php
@@ -0,0 +1,53 @@
+<?php
+include_once("../connect.php");
+$commenting_form = '<form action = "addcomment.php" method = "post">
+   <table width = "310" border = "0" cellspacing = "0" cellpadding = "0">
+      <tr>
+        <td colspan = "2"><strong>Add Comment:</Strong></td>
+      </tr>
+        <tr>
+          <td width = "105">Title:</td>
+          <td width = "205"><input type="text" name="msg_title" id="msg_title style=width:200px;" /></td>
+        </tr>
+        <tr>
+          <td colspan = "2"><textarea name="msg_message" id="msg_message" style="width:100;height:200px;font-family:Courier New"> 
+          Message</textarea></td>
+        </tr>
+        <tr>
+          <td colspan = "2" align = "center"><input type="submit" value="Add Comment" name="msg_submit" id="msg_submit" /></td>
+        </tr>
+    </table>
+    </from>';
+$get_comments = mysql_query("SELECT * FROM 'comments' ORDER BY id DESC");
+$comments_count = mysql_num_rows($get_comments);
+if ($comments_count > 0)
+{
+   while ($com = mysql_fetch_array($get_comments))
+   {
+      $id = $com['id'];
+      $title = $com['text'];
+      $message = $com['message'];
+      $commment .= '<b>'.$title.'</b><br />'.$message.'<hr />';
+   }
+   $comment .= $commenting_form;
+   $page_title = $comments_count.'Comments';
+}
+else
+{
+   $comment = 'There are no comments at the moment.<br />'.commenting_form;
+   $page_title = 'No Comments';
+}
+?>
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhmtl1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+   <title><?php echo $page_title; ?></title>
+</head>
+<body>
+   <?php
+   echo $comment;
+   ?>
+</body>
+</html>
