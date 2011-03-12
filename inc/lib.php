01d956ba00ac64b36eee9e31f011651697d45454
elisehein <elisehein@gmail.com>
2011-03-12 06:19:42 -0800

    add functions to get dob

--- inc/lib.php
+++ inc/lib.php
@@ -137,3 +137,30 @@ function getCurrentDirectory() {
 
 
 ?>
+
+/*
+ * Gets and returns the day of birth of the user from the timestamp
+ */
+function getDayOfBirth() {
+	$dayOfBirth = date("d", $USER['dob'];
+	return $dayOfBirth;
+}
+
+/*
+ * Gets and returns the month of birth of the user from the timestamp
+ */
+function getMonthOfBirth() {
+	$monthOfBirth = date("m", $USER['dob'];
+	return $monthOfBirth;
+}
+
+/*
+ * Gets and returns the year of birth of the user from the timestamp
+ */
+function getYearOfBirth() {
+	$yearOfBirth = date("Y", $USER['dob'];
+	return $yearOfBirth;
+}
+
+
+?>
