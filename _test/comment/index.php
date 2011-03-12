df5b91f5c53a219e5f2c62f21235d79531b309eb
Stephen Hall <halls9@cs.man.ac.uk>
2011-03-12 00:56:57 +0000

    differnet comments system

---
+++ _test/comment/index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+
+
+
+
+
+?>
+
+<html>
+
+<head>
+
+<title>Comment Box</title>
+
+</head>
+
+<body>
+
+<form action="index.php" method="POST".
+<table>
+
+<tr><td>Name: </td><td><input type="text" name="name" /></td></tr>
+<tr><td colspan="2">Comment: </td></tr>
+<tr><td colspan="2"><textarea name="comment"></textarea></td></tr>
+<tr><td colspan="2"><input type="submit" name="submit" value="Comment" /></td></tr>
+
+
+</table>
+
+
+</body>
+</html>
