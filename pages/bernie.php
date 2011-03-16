d607f2de25720024a213a71a5bd3dcbcc3efcb95
Adam <adam-gill@hotmail.co.uk>
2011-03-16 17:32:51 +0000

    Trying alt suggestions

--- pages/bernie.php
+++ pages/bernie.php
@@ -43,54 +43,14 @@ $h=4;
 $suggestionID = 0;
 $suggestionID = $_GET['id'];
 $category = $_GET['c'];
-if ($suggestionID < 1) {
-	
+if ($suggestionID < 1) 
 	$suggestionID = getNewSuggestion($category);
-	
-}
+/* $altSuggestionIDs = getAltSuggestions($suggestionID) */
+$altSuggestionIDs = array(4,5,6);
 
 
-/*
-function getNewSuggestion($category) { 
-
- Go through current users likes, adding every tag and every time it appears to an array 
-
-in the user_interests in the database which record the id's of likes and id's of dislikes.
-
-HAS TO GO THROUGH LIKES FIRST.
-
-then dislikes removes tag id's from the array.
-
-initial interests does something too. 
-
-leaving us with a array of tag ids to choose a random tag id from.
-
-for now lets just make an array with some numbers in 
-
-	$likedTags = array(14,14,14);
-
-	$chosenTag = $likedTags[array_rand($likedTags)];
-
-        $potentialSuggestions = array();
-	$i=0;
-
-	$allSuggestionsOfCategory = dbQuery("SELECT id,tags FROM suggestions WHERE category='$category'");
 
 
-	while($row = mysql_fetch_array($allSuggestionsOfCategory))
-	{
-		$abc = unserialize($row['tags']);	
-		foreach($abc as $someTag) {		
-			if ($someTag=="$chosenTag") {                       		
-				$potentialSuggestions[$i] = $row['id'];				
-				$i+= 1;
-			}
-		}
-	}
-	
-	$suggestionID = $potentialSuggestions[array_rand($potentialSuggestions)];
-	echo $suggestionID;	
-} */
 	
 
 /*/
@@ -98,20 +58,20 @@ for now lets just make an array with some numbers in
  * Also the queries just choose a random row atm
 /*/
 $suggestion1 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,url FROM suggestions WHERE id='$suggestionID'");
-$row5 = mysql_fetch_row($suggestion1);
+$row = mysql_fetch_row($suggestion1);
 
-$suggestion2 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,category FROM suggestions ORDER BY rand() LIMIT 1");
+$suggestion2 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,category FROM suggestions WHERE id='$altSuggestionIDs[0]'");
 $row2 = mysql_fetch_row($suggestion2);
 
-$suggestion3 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,category FROM suggestions ORDER BY rand() LIMIT 1");
+$suggestion3 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,category FROM suggestions WHERE id='$altSuggestionIDs[1]'");
 $row3 = mysql_fetch_row($suggestion3);
 
-$suggestion4 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,category FROM suggestions ORDER BY rand() LIMIT 1");
+$suggestion4 = dbQuery("SELECT id,image_med,title,author,release_year,length,summary,description,category FROM suggestions WHERE id='$altSuggestionIDs[2]'");
 $row4 = mysql_fetch_row($suggestion4);
 
 
-$suggestion= array("sugImage"=>"$row5[1]","sugTitle"=>"$row5[2]","sugAuthor"=>"$row5[3]",
-"sugYear"=>"$row5[4]","sugLength"=>"$row5[5]","sugSubTitle"=>"$row5[6]","sugDescription"=>"$row5[7]","url"=>"$row5[8]",
+$suggestion= array("sugImage"=>"$row[1]","sugTitle"=>"$row[2]","sugAuthor"=>"$row[3]",
+"sugYear"=>"$row[4]","sugLength"=>"$row[5]","sugSubTitle"=>"$row[6]","sugDescription"=>"$row[7]","url"=>"$row[8]",
 "altImage1"=>"$row2[1]","smallAlt1"=>strtolower("$row2[8]"),"altTitle1"=>"$row2[2]","altDisc1"=>truncate("$row2[7]", 85),
 "altImage2"=>"$row3[1]","smallAlt2"=>strtolower("$row3[8]"),"altTitle2"=>"$row3[2]","altDisc2"=>truncate("$row3[7]", 85),
 "altImage3"=>"$row4[1]","smallAlt3"=>strtolower("$row4[8]"),"altTitle3"=>"$row4[2]","altDisc3"=>truncate("$row4[7]", 85));
@@ -123,10 +83,6 @@ $suggestion['sugImage'] = $row[1];
 $suggestion['sugTitle'] = $row[2];
 etc...
 */
-
-
-/* $suggestion= array("sugImage"=>"http://cvcl.mit.edu/hybrid/cat2.jpg","Cat"=>"_","sugTitle"=>"The cat book",
-"sugAuthor"=>"Cat writer","sugYear"=>"1990","sugLength"=>"Long","sugSubTitle"=>"cat the cat cat is a cat which shat","sugDescription"=>"cattycattycatty","altImage1"=>"http://www.cats.org.uk/images/cat_silhouette_news.jpg","smallAlt1"=>"https://github.com/Pezmc/Bernie/raw/master/old/Original%20Files/icon_books_small.png","altTitle1"=>"more cats","altDisc1"=>"oh they are so fluffy","altImage2"=>"http://www.cats.org.uk/images/cat_silhouette_news.jpg","smallAlt2"=>"https://github.com/Pezmc/Bernie/raw/master/old/Original%20Files/icon_books_small.png","altTitle2"=>"more cats","altDisc2"=>"oh they are so fluffy","altImage3"=>"http://www.cats.org.uk/images/cat_silhouette_news.jpg","smallAlt3"=>"https://github.com/Pezmc/Bernie/raw/master/old/Original%20Files/icon_books_small.png","altTitle3"=>"more cats","altDisc3"=>"oh they are so fluffy"); */
 
 
 /* Rest of document just deals with displaying information not getting it */
