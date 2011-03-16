c9a7ba40306234cd6218af4d8acd94c6b59eec38
D1Adam <adam-gill@hotmail.co.uk>
2011-03-15 20:13:03 -0700

    Edited pages/bernie.php via GitHub

--- pages/bernie.php
+++ pages/bernie.php
@@ -76,7 +76,7 @@ while($row = mysql_fetch_array( $usersLikes )
 $currentsuggestiontags =   unserialize{$row['tags]}
 echo "$currentsuggestiontags";
 echo "this is doing something";
- /*
+ 
 /* $suggestionsTags = unserialize($string); 
 if tags contains $rand
 
