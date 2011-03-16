5de3f5020ce1dd51e7d5964dd8d1c4b53120d82c
Elise Hein <elisehein@gmail.com>
2011-03-16 10:59:46 +0200

    adding isset function

--- inc/signup.php
+++ inc/signup.php
@@ -23,11 +23,11 @@ $dob = 98790870; // Use random one for now
 $password = "password"; // Use random one for now
 
 // Other inputs from form
-$gender = sanitise($_POST['gender'], 1);
-$first_name = sanitise($_POST['first_name'], 1);
-$username = sanitise($_POST['username'], 1);
-$parents_name = sanitise($_POST['parents_name'], 1);
-$parents_email = sanitise($_POST['parents_email'], 1);
+if (isset($_POST['gender'])) {$gender = sanitise($_POST['gender'], "1");} else $gender = "";
+if (isset($_POST['first_name'])) $first_name = sanitise($_POST['first_name'], "1"); else $first_name = "";
+if (isset($_POST['username'])) $username = sanitise($_POST['username'], "1"); else $username = "";
+if (isset($_POST['parents_name'])) $parents_name = sanitise($_POST['parents_name'], "1"); else $parents_name = "";
+if (isset($_POST['parents_email'])) $parents_email = sanitise($_POST['parents_email'], "1"); else $parents_email = "";
 
 // Create the user
 dbQuery("INSERT INTO users (gender, first_name, username, dob, parents_name, parents_email, password) 
