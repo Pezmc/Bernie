841bcec08f34cb78dc36a3e11865ff671886cebb
Elise Hein <elisehein@gmail.com>
2011-03-16 11:22:33 +0200

    working on signup

--- inc/signup.php
+++ inc/signup.php
@@ -20,10 +20,10 @@ include_once('database.php');
 // I dont know what im doing
 
 // CODE HERE to convert users date, month and year of birth into a timestamp
-$dob = 98790870; // Use random one for now
+$dob = "98790870"; // Use random one for now
 
 // CODE HERE to generate a nine letter password that will be emailed to the user
-$password = "password"; // Use random one for now
+$password = "randompass"; // Use random one for now
 
 // Other inputs from form
 if (isset($_POST['gender'])) {$gender = sanitise($_POST['gender'], "1");} else $gender = "";
@@ -34,7 +34,7 @@ if (isset($_POST['parents_email'])) $parents_email = sanitise($_POST['parents_em
 
 // Create the user
 dbQuery("INSERT INTO users (gender, first_name, username, dob, parents_name, parents_email, password) 
-				 VALUES ('gender', 'first_name', 'username', 'dob', 'parents_name', 'parents_email', 'password')");
+				 VALUES ('$gender', '$first_name', '$username', '$dob', '$parents_name', '$parents_email', '$password')");
 
 ?>
 
