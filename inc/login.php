ceeac3621fec8d62454ca6ac6f6233b5697824c2
Elise Hein <elisehein@gmail.com>
2011-03-22 11:49:44 +0200

    trying not to overwrite florin's work..

--- inc/login.php
+++ inc/login.php
@@ -12,4 +12,23 @@
 
 /* See the LIB for three LOGIN/LOGOUT related functions */
 
+if (!empty($_POST))
+{
+  $username = sanitise($_POST['login_username'], 1);
+  $result = dbQuery("SELECT * FROM users WHERE username = '$username' LIMIT 1");
+  if (mysql_num_rows($result) == 1)
+  {
+    $row = mysql_fetch_array($result);
+    // Hashed & Salted
+    // if (md5(md5($_POST['login_passw']).$row['salt']) == $row['password'])
+    // Plain
+    if ($_POST['login_passw'] == $row['password'])
+      validateUser($row['id']);
+    else
+      echo 'Error_pass';
+  }
+  else
+    echo 'Error_user';
+}
+
 ?>
