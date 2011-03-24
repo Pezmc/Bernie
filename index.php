58448538742b91688e43cba8e164bfb576db0571
Pez Cuckow <email@pezcuckow.com>
2011-03-24 02:03:30 +0000

    Tidy up

--- index.php
+++ index.php
@@ -12,14 +12,12 @@
  *
 /*/
 
-mail("pegpro@gmail.com", "Test", "Test");
-
 /* ===== God like code from here on in ===== */
 
 /* Show us errors */
-ini_set('display_errors', 1); 
-ini_set('log_errors', 1); 
-error_reporting(E_ALL);
+ini_set('display_errors', 0); 
+ini_set('log_errors', 0); 
+//error_reporting(E_ALL);
 ini_set('mysql.connect_timeout', 10);
 
 /* Allow tracking sessions (logins) */
