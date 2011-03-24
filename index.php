43c7210d27423d266c01c4c8bb0261b57ae08eda
Pez Cuckow <email@pezcuckow.com>
2011-03-24 02:05:06 +0000

    yer

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 /* ===== God like code from here on in ===== */
 
 /* Show us errors */
-ini_set('display_errors', 0); 
+ini_set('display_errors', 1); 
 ini_set('log_errors', 0); 
 //error_reporting(E_ALL);
 ini_set('mysql.connect_timeout', 10);
