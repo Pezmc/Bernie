17673b15a7d5f08ace0290979ab076059dda3553
Elise Hein <elisehein@gmail.com>
2011-03-24 13:58:19 +0200

    lost password

--- pages/lostPassword.php
+++ pages/lostPassword.php
@@ -12,7 +12,7 @@
 /*/
 
 /* Thinking Code */
-include_once('inc/lib.php');
+include_once('inc/lostPassword.php');
 
 /* Rest of document just deals with displaying information not getting it */
 
@@ -20,14 +20,24 @@ $PAGE['title'] = "Lost password";
 
 $PAGE['content'] .= 
 '
+<!-- Display confirmation here in case all ok -->
+		 	{if:!empty(confirmation_message)}
+			  <div id="confirmations"><ul>{confirmation_message}</ul></div>
+		 	{end}
+
+<!-- Display errors here in case there are any -->
+		 	{if:!empty(error_message)}
+			  <div id="errors"><ul>{error_message}</ul></div>
+		 	{end}
+
 <b>Lost password?</b>
 <section class="text" style="padding-top: 15px; padding-bottom: 15px;">
 <p>
 Not to worry, just enter your e-mail address below and we will send you a new one!
 </p>
 </section>
-<form action="" method="" name="">
-<input type="text" placeholder="E-mail address" name="email" maxlength="16">
+<form action="/Bernie/p=lostPassword" method="POST" name="lostpassword">
+<input type="text" placeholder="E-mail address" name="email" maxlength="16" {if:!empty(error_message)}class="error"{end}>
 <input type="submit" class="small" value="GO">
 </form>
 
